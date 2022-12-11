01bcf5704d178fdd0723ddc7431c3d34c8c44855
armansyahhakim <armansyah.hakim@gmail.com>
2022-12-11 18:33:16 +0700

    Add first part of e-commerce using html/css/and javascript

---
+++ javascript.js
@@ -0,0 +1,51 @@
+// // js for toggle menu
+
+// const menuItem = document.querySelector(".menu-item");
+// const menuIcon = document.querySelector(".menu-icon");
+
+// menuItem.style.maxHeight = "0px";
+
+// menuIcon.onclick = () => {
+//     if(menuItem.style.maxHeight == "0px"){
+//         menuItem.style.maxHeight = "200px"; 
+//     } else {
+//         menuItem.style.maxHeight = "0px"; 
+//     }
+// }
+
+// // js for product gallery
+
+// const productImg = document.getElementById("product-img");
+// const smallImg = document.getElementsByClassName("small-img");
+// smallImg[0].onclick = () => 
+// {
+//     productImg.src = smallImg[0].src;
+// }
+// smallImg[1].onclick = () => 
+// {
+//     productImg.src = smallImg[1].src;
+// }
+// smallImg[2].onclick = () => 
+// {
+//     productImg.src = smallImg[2].src;
+// }
+// smallImg[3].onclick = () => 
+// {
+//     productImg.src = smallImg[3].src;
+// }
+
+// // js for toggle form account page
+
+//     const loginForm = document.getElementById("loginForm");
+//     const regForm = document.getElementById("regForm");
+//     const indicator = document.getElementById("indicator");
+
+//         function register(){
+//             regForm.style.transform = "translateX(0px)";
+//             loginForm.style.transform = "translateX(0px)";
+//         }
+
+//         function login(){
+//             regForm.style.transform = "translateX(300px)";
+//             loginForm.style.transform = "translateX(300px)";
+//         }
